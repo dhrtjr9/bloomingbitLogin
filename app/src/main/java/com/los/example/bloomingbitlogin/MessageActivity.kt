ca0068ccf6eb2cad65b9fbca63a63463030d7a7e
dhrtj <dhrtjr@gmail.com>
2023-01-20 01:20:16 +0900

    전달해준 문구를 메세지 화면에서 사용

--- app/src/main/java/com/los/example/bloomingbitlogin/MessageActivity.kt
+++ app/src/main/java/com/los/example/bloomingbitlogin/MessageActivity.kt
@@ -2,10 +2,19 @@ package com.los.example.bloomingbitlogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_message.*
 
 class MessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_message)
+
+        // 이 화면에 들어올때 첨부된 데이터를 텍스트뷰에 반영.
+        // 올 때 뭐 든거 없니?
+
+        val intentMessage = intent.getStringExtra("message")
+
+        receivedMessageTxt.text = intentMessage
+
     }
 }
