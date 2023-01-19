67c6cb444d5ca223c7e497f60dab3c77a0a2e74b
dhrtj <dhrtjr@gmail.com>
2023-01-20 00:09:55 +0900

    Intent 이용 -> 다른 화면으로 이동

--- app/src/main/java/com/los/example/bloomingbitlogin/MainActivity.kt
+++ app/src/main/java/com/los/example/bloomingbitlogin/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.los.example.bloomingbitlogin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -16,7 +17,16 @@ class MainActivity : AppCompatActivity() {
         
         val inputId = idEdt.text.toString()
         val inputPw = passwordEdt.text.toString()
-        
+
+        // 다른 화면으로 이동 (SignActivity)
+        moveToOtherBtn.setOnClickListener {
+
+            val myIntent = Intent(this, SignActivity::class.java)
+            startActivity(myIntent)
+        //출발지 : this / 도착액티비티 :: class.java
+        //실제로는 액티비티 이동이 아니라, 새 핵티비티를 하나 더 만들어서 기존의 액티비티 위에 얹어주는 기능이다.
+        }
+
         //두개의 변수에 들어있는 값 검사. (맞는 아이디/비번)
         //맞을때 -> 틀릴때 분기처리. (if 문 처리)
         
@@ -31,8 +41,9 @@ class MainActivity : AppCompatActivity() {
         else {
             //아이디 내지는 비번 중 하나라도 틀리면 로그인 실패 토스트
             Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
-        }
-    }
+            }
 
+
+        }
     }
 }
--- app/src/main/java/com/los/example/bloomingbitlogin/SignActivity.kt
+++ app/src/main/java/com/los/example/bloomingbitlogin/SignActivity.kt
@@ -1,11 +1,14 @@
 package com.los.example.bloomingbitlogin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class SignActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign)
+
     }
 }
