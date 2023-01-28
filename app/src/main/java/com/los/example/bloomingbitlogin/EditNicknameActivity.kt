bd7dd6d361e31dfb2b63b7adf95150bc7b07fe1d
dhrtj <dhrtjr@gmail.com>
2023-01-29 00:22:55 +0900

    닉네임 변경화면 -> 입력후 완료 클릭 시, 데이터 들고 돌아가기 - 돌아간 메인화면에서 데이터 반영 별도 작업 필요

--- app/src/main/java/com/los/example/bloomingbitlogin/EditNicknameActivity.kt
+++ app/src/main/java/com/los/example/bloomingbitlogin/EditNicknameActivity.kt
@@ -1,5 +1,6 @@
 package com.los.example.bloomingbitlogin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_edit_nickname.*
@@ -8,6 +9,22 @@ class EditNicknameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nickname)
+
+        okBtn.setOnClickListener {
+
+            //입력한 새 닉네임이 뭔지? 변수로 저장해서 사용한다.
+            val inputNewNickname = newNicknameEdt.text.toString()
+
+            //입력한 닉네임을 가지고 메인으로 복귀해야한다.
+            //입력완료는 진짜 정의 / 백버튼은 돌아가는 것
+
+            //입력한 닉네임을 담아주기 위한 용도로만 사용하는 Intent
+            val resultIntent = Intent()
+            resultIntent.putExtra("nickname", inputNewNickname)
+            setResult(RESULT_OK, resultIntent)
+            finish()
+
+        }
 
     }
 }
