5279badf7dd9bc3b929309b4fe594c5729058689
dhrtj <dhrtjr@gmail.com>
2023-01-29 00:09:51 +0900

    닉네임 변경을 위한 화면 이동 (startActivityForResult) 활용

--- app/src/main/java/com/los/example/bloomingbitlogin/SignActivity.kt
+++ app/src/main/java/com/los/example/bloomingbitlogin/SignActivity.kt
@@ -7,9 +7,18 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_sign.*
 
 class SignActivity : AppCompatActivity() {
+
+    val REQUEST_FOR_NICKNAME = 1005
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign)
+
+        editNicknameBtn.setOnClickListener {
+
+            val myIntent = Intent(this, EditNicknameActivity::class.java)
+            startActivityForResult(myIntent,REQUEST_FOR_NICKNAME)
+        }
 
         sendMessageBtn.setOnClickListener {
 
